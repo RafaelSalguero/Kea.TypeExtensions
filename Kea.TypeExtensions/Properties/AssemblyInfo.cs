3ce6226d79d0b6352f4c392dd5089935ea64a175
Rafael Salguero <rafaelsalgueroiturrios@gmail.com>
2016-11-09 12:44:45 -0700

    Type extensions initial commit

--- Kea.TypeExtensions/Properties/AssemblyInfo.cs
+++ Kea.TypeExtensions/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Kea.TypeExtensions")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Type extensions")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Kea software")]
 [assembly: AssemblyProduct("Kea.TypeExtensions")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
